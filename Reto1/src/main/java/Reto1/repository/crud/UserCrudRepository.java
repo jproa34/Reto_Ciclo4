0875f7919687b5accdeb7873669732fe0c942c2d
jproa34 <juan.roa.mt@correo.usa.edu.co>
2021-11-25 17:15:59 -0500

    Backend reto1

---
+++ Reto1/src/main/java/Reto1/repository/crud/UserCrudRepository.java
@@ -0,0 +1,15 @@
+package Reto1.repository.crud;
+
+import Reto1.model.User;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Juan Pablo Roa Fragozo
+ */
+
+public interface UserCrudRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email,String password);
+    //Optional<User> findByName(String name);
+}
