408d582fc897699352243b991f2f0f4e1e5586e4
jproa34 <juan.roa.mt@correo.usa.edu.co>
2021-12-04 17:27:06 -0500

    Backen listo

---
+++ src/main/java/Reto1/Reto1Application.java
@@ -0,0 +1,19 @@
+   
+package Reto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author Juan Pablo Roa
+ */
+
+@SpringBootApplication
+public class Reto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1Application.class, args);
+	}
+
+}
